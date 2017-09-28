62edc4ddd69c3c4ee9c1376c1a520c29edcca78f
Izabela Guerreiro <izaguerreiro@gmail.com>
2017-09-28 17:35:07 -0300

    remove useless command

--- contrib.py
+++ contrib.py
@@ -39,7 +39,6 @@ def get_args():
 
     parser.add_argument('-repo', type=str,help='Name of the Repository\nIf repository in id then type only the REPO_NAME\nIf repository in Organisation type: ORG_NAME/REPO_NAME',required=True)
     args = parser.parse_args()
-    #print type(args.repo)
     return args
 
 
@@ -78,7 +77,7 @@ try:
             names.append(g.get_user(str(member.login)).name)
 except Exception as e:
     sys.exit(e)
-#print members
+
 cwd = os.getcwd()
 
 list_dir = os.listdir(cwd)
@@ -86,14 +85,9 @@ list_dir = os.listdir(cwd)
 folder = 'rpy'
 
 if folder in list_dir:
-    #print 'Changing Folder'
     os.chdir(os.getcwd() + "/" + folder)
-    #print 'Done'
 
 cwd = os.getcwd()
-
-#list_dir = os.listdir(cwd)
-
 
 
 with open('head.rpy', 'r') as content_file:
@@ -108,7 +102,7 @@ repo = '<%= repo %>'
 name = '<%= name %>'
 replace = {repo:repository,uid:members,name:names}
 collaborators = replace[uid]
-#print collaborators
+
 with open('Contributors.md', 'w') as outfile:
     outfile.write(head)
     for i in range(len(replace[uid])):
